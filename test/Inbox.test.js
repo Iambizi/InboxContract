1bc0a464762d29db8760396c27461bfc9e75c98c
Iambizi <amirbzmn@gmail.com>
2022-06-10 16:00:54 -0400

    Set up test file and mocha testing overview

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,17 @@
 // contract test code will go here
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = reqeuire('web3');
+// Capital casing of W refers to constructor
+const Web3 = require('web3');
+//Lower casing of w refers to instace
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom vroom';
+    }
+}
